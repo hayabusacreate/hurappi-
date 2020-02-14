cd97b9cecf94c924f29738cfcb1e6218b61070d9
大石　隼 <>
2020-02-14 21:10:34 +0900

    aaaa

---
+++ Flappy bird/Assets/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //ジャンプ力
+    public float jumpPower=5;
+    private Rigidbody rigidbody;
+    private Vector3 JumpPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //PlayerのRigidbodyを代入
+        rigidbody = gameObject.transform.GetComponent<Rigidbody>();
+        //ジャンプ力をrigidbody.velocityに代入するためにVector3に変換
+        JumpPos = new Vector3(0, jumpPower, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //スペース押したらジャンプ
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidbody.velocity = JumpPos;
+        }
+    }
+}
